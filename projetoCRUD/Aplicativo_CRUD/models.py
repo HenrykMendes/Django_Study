277f69e0e2c3c6d25f105e2a60c968c07df9682b
João Henrique Mendes <h06.987654321@gmail.com>
2024-11-01 17:56:31 -0300

    Criação das tabelas do "Models" e migrando para o db.sqlite3

--- projetoCRUD/Aplicativo_CRUD/models.py
+++ projetoCRUD/Aplicativo_CRUD/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class item (models.Model):
+    #Campos que vão ser criados do database
+    nome = models.CharField (max_length=100)
+    descricao = models.TextField
+    preco = models.DecimalField (max_digits=10, decimal_places=2)
+    data = models.DateTimeField(auto_now_add=True)
